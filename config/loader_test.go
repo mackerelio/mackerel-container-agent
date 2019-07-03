c49d98b54376f58b37ce4621c48a6c881f5bc166
itchyny <itchyny@hatena.ne.jp>
2019-07-03 15:43:21 +0900

    add test for config loader

---
+++ config/loader_test.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestLoaderLoad(t *testing.T) {
+	file, err := newConfigFile(`
+apikey: 'DUMMY APIKEY'
+root: '/tmp/mackerel-container-agent'
+`)
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	defer os.Remove(file.Name())
+
+	expect := &Config{
+		Apibase: "",
+		Apikey:  "DUMMY APIKEY",
+		Root:    "/tmp/mackerel-container-agent",
+	}
+
+	confLoader, err := NewLoader(file.Name(), "")
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	conf, err := confLoader.Load()
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	if !reflect.DeepEqual(conf, expect) {
+		t.Errorf("expect %#v, got %#v", expect, conf)
+	}
+}
