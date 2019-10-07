d70c98e6ba8d6b2e6001c6623655746a366113f7
lufia <lufia@lufia.org>
2019-10-07 15:06:23 +0900

    use /tmp for generating types

--- local-apis/update.sh
+++ local-apis/update.sh
@@ -2,9 +2,10 @@
 
 set -eu
 
-cd $(dirname $0)
-rm -rf kubernetes
-git clone --depth=1 https://github.com/kubernetes/kubernetes
+d=$(mktemp -d)
+trap 'rm -rf $d; exit 1' 1 2 3 15
+
+git clone --depth=1 https://github.com/kubernetes/kubernetes $d
 rm -rf stats
-cp -R kubernetes/pkg/kubelet/apis/stats .
-rm -rf kubernetes
+cp -R $d/pkg/kubelet/apis/stats .
+rm -rf $d
