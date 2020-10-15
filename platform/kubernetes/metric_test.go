76b971ba9fe1ecb9a6d117ee4710568e24b8684f
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-10-15 12:39:30 +0900

    add test which fallbacks to getNode

--- platform/kubernetes/metric_test.go
+++ platform/kubernetes/metric_test.go
@@ -89,6 +89,81 @@ func TestGenerateStats(t *testing.T) {
 	}
 }
 
+func TestGenerateStats_FallbackToNodeAPI(t *testing.T) {
+	ctx := context.Background()
+	client := kubelet.NewMockClient(
+		kubelet.MockGetPod(func(context.Context) (*kubernetesTypes.Pod, error) {
+			raw, err := ioutil.ReadFile("kubelet/testdata/pods.json")
+			if err != nil {
+				return nil, err
+			}
+			var podList kubernetesTypes.PodList
+			if err := json.Unmarshal(raw, &podList); err != nil {
+				return nil, err
+			}
+			for _, pod := range podList.Items {
+				if pod.ObjectMeta.Namespace == "default" && pod.ObjectMeta.Name == "myapp" {
+					return &pod, nil
+				}
+			}
+			return nil, nil
+		}),
+		kubelet.MockGetPodStats(func(context.Context) (*kubeletTypes.PodStats, error) {
+			raw, err := ioutil.ReadFile("kubelet/testdata/summary.json")
+			if err != nil {
+				return nil, err
+			}
+			var summary kubeletTypes.Summary
+			if err := json.Unmarshal(raw, &summary); err != nil {
+				return nil, err
+			}
+			for _, pod := range summary.Pods {
+				if pod.PodRef.Namespace == "default" && pod.PodRef.Name == "myapp" {
+					return &pod, nil
+				}
+			}
+			return nil, nil
+		}),
+		kubelet.MockGetSpec(func(context.Context) (*cadvisorTypes.MachineInfo, error) {
+			return nil, kubelet.ErrNotFound
+		}),
+	)
+	serviceClient := kubernetesapi.NewMockClient(
+		kubernetesapi.MockGetNode(func(context.Context) (*kubernetesTypes.Node, error) {
+			raw, err := ioutil.ReadFile("kubernetesapi/testdata/node.json")
+			if err != nil {
+				return nil, err
+			}
+			var info kubernetesTypes.Node
+			if err := json.Unmarshal(raw, &info); err != nil {
+				return nil, err
+			}
+			return &info, nil
+		}),
+	)
+	generator := newMetricGenerator(client, serviceClient)
+	_, err := generator.Generate(ctx) // Store metrics to generator.prevStats.
+	if err != nil {
+		t.Errorf("Generate() should not raise error: %v", err)
+	}
+	got, err := generator.Generate(ctx)
+	if err != nil {
+		t.Errorf("Generate() should not raise error: %v", err)
+	}
+	expected := metric.Values{
+		"container.cpu.mackerel-container-agent.usage":    0.0, // Result is 0 because use the same data.
+		"container.cpu.nginx.usage":                       0.0, // Result is 0 because use the same data.
+		"container.cpu.mackerel-container-agent.limit":    25.0,
+		"container.cpu.nginx.limit":                       100.0, // taken from Node API
+		"container.memory.mackerel-container-agent.usage": 2.6529792e+07,
+		"container.memory.nginx.usage":                    1.949696e+06,
+		"container.memory.mackerel-container-agent.limit": 134217728.0,  // 128MiB
+		"container.memory.nginx.limit":                    3876802560.0, // taken from Node API
+	}
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("Generate() expected %v, got %v", expected, got)
+	}
+}
 func TestGetMemoryLimit(t *testing.T) {
 	hostMemTotal := 2096058368.0
 	name := "dummy"
