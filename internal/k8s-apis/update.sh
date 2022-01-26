7ac9ec07421c532244b086b0369599569fcd8f22
yseto <yseto@users.noreply.github.com>
2022-01-26 20:50:05 +0900

    update internal/k8s-apis

--- internal/k8s-apis/update.sh
+++ internal/k8s-apis/update.sh
@@ -8,6 +8,6 @@ trap 'rm -rf $d; exit 1' 1 2 3 15
 
 git clone --depth=1 https://github.com/kubernetes/kubernetes $d
 rm -rf stats
-cp -R $d/pkg/kubelet/apis/stats .
+cp -R $d/staging/src/k8s.io/kubelet/pkg/apis/stats .
 cp $d/LICENSE .
 rm -rf $d
