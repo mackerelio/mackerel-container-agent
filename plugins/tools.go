26548ba0fd0f5efb4fcf32130bc01808d977e3c9
yseto <yseto@users.noreply.github.com>
2025-08-08 19:18:50 +0900

    fix plugins/go.mod

---
+++ plugins/tools.go
@@ -0,0 +1,8 @@
+package plugins
+
+import (
+	_ "github.com/mackerelio/go-check-plugins"
+	_ "github.com/mackerelio/mackerel-agent-plugins"
+	_ "github.com/mackerelio/mackerel-plugin-json"
+	_ "github.com/mackerelio/mkr"
+)
